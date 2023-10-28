bb71801ec4e20ba6419090f767efe2969257dcdb
[전시개발팀]이창근 <changgeun.lee@lotte.net>
2023-10-28 16:55:00 +0900

    refact: 조건문을 다형성으로 바꾸기
    > getCharge, getFrequentRenterPoints 를 Movie 로 옮김

--- src/main/java/com/example/study_refactoring/example/model/Movie.java
+++ src/main/java/com/example/study_refactoring/example/model/Movie.java
@@ -1,6 +1,7 @@
 package com.example.study_refactoring.example.model;
 
 public class Movie {
+
     public static final int CHILDREN = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
@@ -27,5 +28,37 @@ public class Movie {
 
     public void set_priceCode(int _priceCode) {
         this._priceCode = _priceCode;
+    }
+
+    public double getCharge(int daysRented) {
+        int result = 0;
+
+        switch (get_priceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2) {
+                    result += (daysRented - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDREN:
+                result += 1.5;
+                if (daysRented > 3) {
+                    result += (daysRented - 3) * 1.5;
+                }
+                break;
+        }
+        return result;
+    }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        if ((get_priceCode() == Movie.NEW_RELEASE)
+            && daysRented > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
     }
 }
