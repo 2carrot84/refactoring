e4b42dc052d1b7c92477a5f10109140507792afc
[전시개발팀]이창근 <changgeun.lee@lotte.net>
2023-10-28 16:03:42 +0900

    test: test 코드 추가 작성
    > 렌탈한 케이스 추가

--- src/test/java/com/example/study_refactoring/example/model/CustomerTest.java
+++ src/test/java/com/example/study_refactoring/example/model/CustomerTest.java
@@ -13,13 +13,24 @@ public class CustomerTest {
             + "Amount owed is 0.0\n"
             + "You earned 0 frequent renter points");
 
-        Rental rental = new Rental(new Movie("movie1", Movie.REGULAR), 4);
+        Rental rental1 = new Rental(new Movie("movie1", Movie.REGULAR), 4);
 
-        customer.addRental(rental);
+        customer.addRental(rental1);
 
         assertThat(customer.statement()).isEqualTo("Rental Record for 2carrot\n"
             + "\tmovie1\t5.0\n"
             + "Amount owed is 5.0\n"
             + "You earned 1 frequent renter points");
+
+
+        Rental rental2 = new Rental(new Movie("movie2", Movie.CHILDREN), 1);
+
+        customer.addRental(rental2);
+
+        assertThat(customer.statement()).isEqualTo("Rental Record for 2carrot\n"
+            + "\tmovie1\t5.0\n"
+            + "\tmovie2\t1.0\n"
+            + "Amount owed is 6.0\n"
+            + "You earned 2 frequent renter points");
     }
 }
