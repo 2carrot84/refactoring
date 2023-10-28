c77705b335bff80a5e32e40232ce5235c89feb06
[전시개발팀]이창근 <changgeun.lee@lotte.net>
2023-10-28 17:19:53 +0900

    refact: 조건문을 다형성으로 바꾸기
    > getFrequentRenterPoints NewReleasePrice 에 생성

--- src/main/java/com/example/study_refactoring/example/model/Price.java
+++ src/main/java/com/example/study_refactoring/example/model/Price.java
@@ -6,11 +6,6 @@ abstract public class Price {
     abstract double getCharge(int daysRented);
 
     public int getFrequentRenterPoints(int daysRented) {
-        if ((getPriceCode() == Movie.NEW_RELEASE)
-            && daysRented > 1) {
-            return 2;
-        } else {
-            return 1;
-        }
+        return 1;
     }
 }
