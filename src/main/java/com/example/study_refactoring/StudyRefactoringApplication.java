33514388bfa05903bd3090e61eca79ebd8e6b9c8
[전시개발팀]이창근 <changgeun.lee@lotte.net>
2023-10-28 14:44:35 +0900

    feat: init

---
+++ src/main/java/com/example/study_refactoring/StudyRefactoringApplication.java
@@ -0,0 +1,13 @@
+package com.example.study_refactoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyRefactoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyRefactoringApplication.class, args);
+    }
+
+}
